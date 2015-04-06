569375556272dd09f2858baee2a033f3d2d138c8
wSedlacek <w@sedlaceks.org>
2015-04-05 19:36:10 -0700

    Update 1

--- SkyrimRandomGeneratorWithMass/Props.h
+++ SkyrimRandomGeneratorWithMass/Props.h
@@ -1,2 +1,69 @@
+//General
 enum Race {ALTMER, ARGONIAN, BOSMER, BRETON, DUNMER, IMPERIAL, KHAJIIT, NORD, ORC, REDGAURD};
 enum Gender {MALE, FEMALE};
+enum Age {YOUNG, ADULT, MATURE, MIDDLEAGED, ELDER, VENERABLE, AGELESS};
+enum Profession {ACROBAT, AGENT, ARCHER, ASSASSIN, BARBARIAN, BARD, BATTLEMAGE, CRUSADER, HEALER, KNIGHT, MAGE, MONK, NIGHTBLADE, PILGRIM, ROGUE, SCOUT, SORCERER, SPELLSWORD, THIEF, WARRIOR, WITCHHUNTER};
+enum BirthSign { THEAPPRENTICE, THEATRONACH, THELADY, THELORD, THELOVER, THEMAGE, THERITUAL, THESERPENT, THESHADOW, THESTEED, THETHIEF, THETOWER, THEWARRIOR};
+enum Supernatural {NONE, VAMPIRE, WAREWOLF};
+enum Apparel {CLOTHING, ROBES, LIGHTARMOUR, HEAVYARMOUR};
+enum CombatStyle {MARKSMAN, ONEHANDED, TWOHANDED, DESTRUCTION};
+
+//Skills
+enum MainSkill {};
+enum SecondarySkill {};
+enum CraftingSkill {};
+
+//Personalality
+enum Ethics {};
+enum Morality {};
+enum Temperment {};
+enum Tendency {};
+enum Demeanor {};
+enum Sanity {};
+enum Motivation {};
+enum Deity {};
+enum Piety {};
+enum Habit {};
+enum Hobby {};
+enum Interest {};
+
+//Appearance
+enum Weight {};
+enum Skintone {};
+enum Complexion {};
+enum Scars {};
+enum Warpaint {};
+enum WarpaintColor {};
+enum EyeColor {};
+enum HairSyle {};
+enum FacialHair {};
+enum HairColor {};
+
+//Racism
+enum AltmerFeelings {};
+enum ArgonianFeelings {};
+enum BosmerFeelings {};
+enum BretonFeelings {};
+enum DunmerFeelings {};
+enum ImperialFeelings {};
+enum KhajiitFeelings {};
+enum NordFeelings {};
+enum OrcFeelings {};
+enum RedguardFeelings {};
+
+//Agism
+enum ChildrenFeelings {};
+enum EldersFeelings {};
+
+//Factions
+enum Blades {};
+enum GreyBeards {};
+enum Imperials {};
+enum Stormcloaks {};
+enum Companions {};
+enum DarkBrotherhood {};
+enum MageGuild {};
+enum ThievesGuild {};
+enum BardsGuild {};
+enum Dawnguard {};
+enum VolkiharClan {};
--- SkyrimRandomGeneratorWithMass/RandomVals.h
+++ SkyrimRandomGeneratorWithMass/RandomVals.h
@@ -1,13 +1,82 @@
-int RALTMER    = 10;
-int RARGONIAN  = 10;
-int RBOSMER    = 10;
-int RBRETON    = 10;
-int RDUNMER    = 10;
-int RIMPERIAL  = 10;
-int RKHAJIIT   = 10;
-int RNORD      = 10;
-int RORC       = 10;
-int RREDGAURD  = 10;
+//***GENERAL***
+//RACE
+int RALTMER         = 10;
+int RARGONIAN       = 10;
+int RBOSMER         = 10;
+int RBRETON         = 10;
+int RDUNMER         = 10;
+int RIMPERIAL       = 10;
+int RKHAJIIT        = 10;
+int RNORD           = 10;
+int RORC            = 10;
+int RREDGAURD       = 10;
 
-int RMALE   = 50;
-int RFEMALE = 50;
+//GENDER
+int RMALE           = 50;
+int RFEMALE         = 50;
+
+//AGE
+int RYOUNG          = 15;
+int RADULT          = 15;
+int RMATURE         = 15;
+int RMIDDLEAGED     = 15;
+int RELDER          = 15;
+int RVENERABLE      = 15;
+int RAGELESS        = 10;
+
+//PROFESSION
+int RACROBAT        = 4.8;
+int RAGENT          = 4.8;
+int RARCHER         = 4.8;
+int RASSASSIN       = 4.8;
+int RBARBARIAN      = 4.8;
+int RBARD           = 4.8;
+int RBATTLEMAGE     = 4.8;
+int RCRUSADER       = 4.8;
+int RHEALER         = 4.8;
+int RKNIGHT         = 4.8;
+int RMAGE           = 4.8;
+int RMONK           = 4.8;
+int RNIGHTBLADE     = 4.8;
+int RPILGRIM        = 4.8;
+int RROGUE          = 4.8;
+int RSCOUT          = 4.8;
+int RSORCERER       = 4.8;
+int RSPELLSWORD     = 4.8;
+int RTHIEF          = 4.8;
+int RWARRIOR        = 4;
+int RWITCHHUNTER    = 4.8;
+
+//BIRTHSIGN
+int RTHEAPPRENTICE  = 
+int RTHEATRONACH    =
+int RTHELADY
+int RTHELORD
+int RTHELOVER
+int RTHEMAGE
+int RTHERITUAL
+int RTHESERPENT
+int RTHESHADOW
+int RTHESTEED
+int RTHETHIEF
+int RTHETOWER
+int RTHEWARRIOR
+
+//SUPERNATURAL
+int RNONE           = 70;
+int RVAMPIRE        = 15;
+int RWAREWOLF       = 15;
+
+//APPAREL
+int RCLOTHING       = 25;
+int RROBES          = 25;
+int RLIGHTARMOUR    = 25;
+int RHEAVYARMOUR    = 25;
+
+//COMBATSYLE
+int RMARKSMAN       = 25;
+int RONEHANDED      = 25;
+int RTWOHANDED      = 25;
+int RDESTRUCTION    = 25;
+
+//***SKILLS***

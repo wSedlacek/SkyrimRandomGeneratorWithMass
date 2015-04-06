166dd2fe512d76ceab030b56cd343b3733c3ecda
wSedlacek <w@sedlaceks.org>
2015-04-05 21:10:48 -0700

    Update 1 PART 2

--- SkyrimRandomGeneratorWithMass/Props.h
+++ SkyrimRandomGeneratorWithMass/Props.h
@@ -1,69 +1,69 @@
 //General
-enum Race {ALTMER, ARGONIAN, BOSMER, BRETON, DUNMER, IMPERIAL, KHAJIIT, NORD, ORC, REDGAURD};
-enum Gender {MALE, FEMALE};
-enum Age {YOUNG, ADULT, MATURE, MIDDLEAGED, ELDER, VENERABLE, AGELESS};
-enum Profession {ACROBAT, AGENT, ARCHER, ASSASSIN, BARBARIAN, BARD, BATTLEMAGE, CRUSADER, HEALER, KNIGHT, MAGE, MONK, NIGHTBLADE, PILGRIM, ROGUE, SCOUT, SORCERER, SPELLSWORD, THIEF, WARRIOR, WITCHHUNTER};
-enum BirthSign { THEAPPRENTICE, THEATRONACH, THELADY, THELORD, THELOVER, THEMAGE, THERITUAL, THESERPENT, THESHADOW, THESTEED, THETHIEF, THETOWER, THEWARRIOR};
-enum Supernatural {NONE, VAMPIRE, WAREWOLF};
-enum Apparel {CLOTHING, ROBES, LIGHTARMOUR, HEAVYARMOUR};
-enum CombatStyle {MARKSMAN, ONEHANDED, TWOHANDED, DESTRUCTION};
+enum Race               { ALTMER, ARGONIAN, BOSMER, BRETON, DUNMER, IMPERIAL, KHAJIIT, NORD, ORC, REDGAURD };
+enum Gender             { MALE, FEMALE };
+enum Age                { YOUNG, ADULT, MATURE, MIDDLEAGED, ELDER, VENERABLE, AGELESS};
+enum Profession         { ACROBAT, AGENT, ARCHER, ASSASSIN, BARBARIAN, BARD, BATTLEMAGE, CRUSADER, HEALER, KNIGHT, MAGE, MONK, NIGHTBLADE, PILGRIM, ROGUE, SCOUT, SORCERER, SPELLSWORD, THIEF, WARRIOR, WITCHHUNTER };
+enum BirthSign          { THEAPPRENTICE, THEATRONACH, THELADY, THELORD, THELOVER, THEMAGE, THERITUAL, THESERPENT, THESHADOW, THESTEED, THETHIEF, THETOWER, THEWARRIOR };
+enum Supernatural       { NONE, VAMPIRE, WAREWOLF };
+enum Apparel            { CLOTHING, ROBES, LIGHTARMOUR, HEAVYARMOUR };
+enum CombatStyle        { MARKSMAN, ONEHANDED, TWOHANDED, DESTRUCTION };
 
 //Skills
-enum MainSkill {};
-enum SecondarySkill {};
-enum CraftingSkill {};
+enum MainSkill          { ALTERATION, CONJURATION, DESTRUCTION, ILLUSION, RESTORATION, RANGED, BLOCK, HEAVYARMOR, ONEHANDED, LIGHTARMOR, DEXTERITY, WAYFARER, SNEAK, SPEACH, TWOHANDED };
+enum SecondarySkill     { ALTERATION, CONJURATION, DESTRUCTION, ILLUSION, RESTORATION, RANGED, BLOCK, HEAVYARMOR, ONEHANDED, LIGHTARMOR, DEXTERITY, WAYFARER, SNEAK, SPEACH, TWOHANDED };
+enum CraftingSkill      { ENCHANTING, SMITHING, ALCHEMY };
 
 //Personalality
-enum Ethics {};
-enum Morality {};
-enum Temperment {};
-enum Tendency {};
-enum Demeanor {};
-enum Sanity {};
-enum Motivation {};
-enum Deity {};
-enum Piety {};
-enum Habit {};
-enum Hobby {};
-enum Interest {};
+enum Ethics             {};
+enum Morality           {};
+enum Temperment         {};
+enum Tendency           {};
+enum Demeanor           {};
+enum Sanity             {};
+enum Motivation         {};
+enum Deity              {};
+enum Piety              {};
+enum Habit              {};
+enum Hobby              {};
+enum Interest           {};
 
 //Appearance
-enum Weight {};
-enum Skintone {};
-enum Complexion {};
-enum Scars {};
-enum Warpaint {};
-enum WarpaintColor {};
-enum EyeColor {};
-enum HairSyle {};
-enum FacialHair {};
-enum HairColor {};
+enum Weight             {};
+enum Skintone           {};
+enum Complexion         {};
+enum Scars              {};
+enum Warpaint           {};
+enum WarpaintColor      {};
+enum EyeColor           {};
+enum HairSyle           {};
+enum FacialHair         {};
+enum HairColor          {};
 
 //Racism
-enum AltmerFeelings {};
-enum ArgonianFeelings {};
-enum BosmerFeelings {};
-enum BretonFeelings {};
-enum DunmerFeelings {};
-enum ImperialFeelings {};
-enum KhajiitFeelings {};
-enum NordFeelings {};
-enum OrcFeelings {};
-enum RedguardFeelings {};
+enum AltmerFeelings     { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum ArgonianFeelings   { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum BosmerFeelings     { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum BretonFeelings     { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum DunmerFeelings     { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum ImperialFeelings   { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum KhajiitFeelings    { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum NordFeelings       { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum OrcFeelings        { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum RedguardFeelings   { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
 
 //Agism
-enum ChildrenFeelings {};
-enum EldersFeelings {};
+enum ChildrenFeelings  { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum EldersFeelings    { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
 
 //Factions
-enum Blades {};
-enum GreyBeards {};
-enum Imperials {};
-enum Stormcloaks {};
-enum Companions {};
-enum DarkBrotherhood {};
-enum MageGuild {};
-enum ThievesGuild {};
-enum BardsGuild {};
-enum Dawnguard {};
-enum VolkiharClan {};
+enum Blades           { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum GreyBeards       { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum Imperials        { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum Stormcloaks      { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum Companions       { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum DarkBrotherhood  { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum MageGuild        { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum ThievesGuild     { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum BardsGuild       { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum Dawnguard        { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
+enum VolkiharClan     { HOSTILE, DISLIKE, TOLERANT, INDIFFERENT, FRIENDLY, ADORING, ZEALOT };
--- SkyrimRandomGeneratorWithMass/RandomVals.h
+++ SkyrimRandomGeneratorWithMass/RandomVals.h
@@ -33,34 +33,34 @@ int RBARBARIAN      = 4.8;
 int RBARD           = 4.8;
 int RBATTLEMAGE     = 4.8;
 int RCRUSADER       = 4.8;
-int RHEALER         = 4.8;
+int RHEALER         = 4.7;
 int RKNIGHT         = 4.8;
-int RMAGE           = 4.8;
+int RMAGE           = 4.7;
 int RMONK           = 4.8;
 int RNIGHTBLADE     = 4.8;
 int RPILGRIM        = 4.8;
-int RROGUE          = 4.8;
-int RSCOUT          = 4.8;
-int RSORCERER       = 4.8;
-int RSPELLSWORD     = 4.8;
-int RTHIEF          = 4.8;
-int RWARRIOR        = 4;
+int RROGUE          = 4.7;
+int RSCOUT          = 4.7;
+int RSORCERER       = 4.7;
+int RSPELLSWORD     = 4.7;
+int RTHIEF          = 4.7;
+int RWARRIOR        = 4.7;
 int RWITCHHUNTER    = 4.8;
 
 //BIRTHSIGN
-int RTHEAPPRENTICE  = 
-int RTHEATRONACH    =
-int RTHELADY
-int RTHELORD
-int RTHELOVER
-int RTHEMAGE
-int RTHERITUAL
-int RTHESERPENT
-int RTHESHADOW
-int RTHESTEED
-int RTHETHIEF
-int RTHETOWER
-int RTHEWARRIOR
+int RTHEAPPRENTICE  = 9;
+int RTHEATRONACH    = 8;
+int RTHELADY        = 8;
+int RTHELORD        = 8;
+int RTHELOVER       = 8;
+int RTHEMAGE        = 9;
+int RTHERITUAL      = 8;
+int RTHESERPENT     = 8;
+int RTHESHADOW      = 8;
+int RTHESTEED       = 8;
+int RTHETHIEF       = 9;
+int RTHETOWER       = 8;
+int RTHEWARRIOR     = 9;
 
 //SUPERNATURAL
 int RNONE           = 70;
@@ -80,3 +80,43 @@ int RTWOHANDED      = 25;
 int RDESTRUCTION    = 25;
 
 //***SKILLS***
+//MAIN
+int RSMALTERATION   = 6.6;
+int RSMCONJURATION  = 6.6;
+int RSMDESTRUCTION  = 6.7;
+int RSMILLUSION     = 6.6;
+int RSMRESTORATION  = 6.6;
+int RSMRANGED       = 6.7;
+int RSMBLOCK        = 6.6;
+int RSMHEAVYARMOR   = 6.6;
+int RSMONEHANDED    = 6.7;
+int RSMLIGHTARMOR   = 6.6;
+int RSMDEXTERITY    = 6.6;
+int RSMWAYFARER     = 6.6;
+int RSMSNEAK        = 6.7;
+int RSMSPEACH       = 6.6;
+int RSMTWOHANDED    = 6.7;
+
+//SECONDARY
+int RSSALTERATION   = 6.6;
+int RSSCONJURATION  = 6.6;
+int RSSDESTRUCTION  = 6.6;
+int RSSILLUSION     = 6.6;
+int RSSRESTORATION  = 6.6;
+int RSSRANGED       = 6.6;
+int RSSBLOCK        = 6.7;
+int RSSHEAVYARMOR   = 6.7;
+int RSSONEHANDED    = 6.6;
+int RSSLIGHTARMOR   = 6.7;
+int RSSDEXTERITY    = 6.7;
+int RSSWAYFARER     = 6.6;
+int RSSSNEAK        = 6.6;
+int RSSSPEACH       = 6.7;
+int RSSTWOHANDED    = 6.6;
+
+//CRAFTING
+int RSCENCHANTING   = 30;
+int RSCSMITHING     = 40;
+int RSCALCHEMY      = 30;
+
+//***PERSONALITY***
